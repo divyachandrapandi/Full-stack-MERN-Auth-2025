bc5991f6c2c317f400dd75cd4f812502ce35d2db
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-24 15:47:19 +0530

    Added middleware and Singup

--- api/index.js
+++ api/index.js
@@ -1,12 +1,33 @@
 import express from 'express';
-import connectToDatabase from "./database/mongodb.js";
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import connectToDatabase from "./database/mongodb.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 
 app.listen(3000, async () => {
     console.log("Server listening in 3000");
     await connectToDatabase()
 })
+
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
